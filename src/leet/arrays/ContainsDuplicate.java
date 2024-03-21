a503624d7dd644d160d454dadc0c1786236a7f77
dranzey <drk_dranzer@hotmail.com>
2024-03-20 21:44:05 -0600

    test

--- src/leet/arrays/ContainsDuplicate.java
+++ src/leet/arrays/ContainsDuplicate.java
@@ -21,7 +21,7 @@ public class ContainsDuplicate
      * Given an integer array nums, return true if any value appears at least twice in the array,
      * and return false if every element is distinct.
      */
-    static boolean containsDuplicate(int[] nums)
+    static boolean containsDuplicate(int[] numss)
     {
         /**
          * Brute force
